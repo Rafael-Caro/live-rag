0bbc496e24cec248fed7fab78f84a6efc2f1d581
Rafael Caro Repetto <rafael.caro.repetto@gmail.com>
2019-05-10 18:14:04 +0200

    Working version of rag. Added tracks

--- scripts/liveRagSketch.js
+++ scripts/liveRagSketch.js
@@ -8,29 +8,24 @@ var backColorTrans;
 var frontColor;
 var shadeColor;
 
-var recordingsInfo;
+var ragInfo;
 var recordingsList;
 var pitchSpace;
 var svaraList = [];
-var soundList = {};
 var svaraRadius1 = 20;
 var svaraRadius2 = 17;
-var svaraLine = 50;
+var svaraLine = 70;
 var minHz;
 var maxHz;
 var pitchTrack;
 var trackFile;
 var track;
 var trackDuration;
-var rag;
-var artist;
-var link;
+var ragName;
 
-var selectMenu;
+var ragMenu;
+var recordingMenu;
 var buttonPlay;
-var showTheka;
-var showTal;
-var showCursor;
 
 var cursorTop;
 var cursorBottom;
@@ -39,12 +34,9 @@ var navBoxH = 50;
 var navCursor;
 var navBox;
 var navCursorW = 4;
-var samList = [];
 var melCursorX;
 var clock;
-var mpmTxt;
 
-var talInfo;
 var talCursor;
 var talX;
 var talY;
@@ -65,14 +57,6 @@ var paused = true;
 var charger;
 var currentTime = 0;
 var jump;
-// Language
-var lang_select;
-var lang_load;
-var lang_error;
-var lang_start;
-var lang_pause;
-var lang_continue;
-var lang_loading;
 
 function preload() {
   ragInfo = loadJSON("files/ragInfo.json");
@@ -99,86 +83,68 @@ function setup () {
   frontColor = color(120, 0, 0);
   shadeColor = color(120, 0, 0);
 
-  charger = new CreateCharger();
-  navBox = new CreateNavigationBox();
-  navCursor = new CreateNavCursor();
-  talCursor = new CreateTalCursor();
+  cursorTop = extraSpaceH + margin*3 + 50;
+  cursorBottom = height - margin*3;
+  melCursorX = extraSpaceW + (mainSpace / 2);
 
-  cursorTop = extraSpaceH + margin*7 + 50;
-  cursorBottom = navBox.y1-margin*4;
-  talX = extraSpaceW + margin + (mainSpace-2*margin)/3;
-  talY = cursorTop + (cursorBottom-cursorTop)/2.8 + strokeRadius1/2;
-  talRadius = (cursorBottom-cursorTop)/2.8;// (mainSpace-2*margin)*0.25;
-  melCursorX = extraSpaceW + (mainSpace-2*margin)*0.75;
-
-  //Language
-  var lang = select("html").elt.lang;
-  print(lang);
-  if (lang == "es") {
-    lang_load = "Carga el audio";
-    lang_select = "Elige";
-    lang_error = "Ha habido un problema cargando el audio\nPor favor, vuelve a cargar la página";
-    lang_start = "¡Comienza!";
-    lang_pause = "Pausa";
-    lang_continue = "Sigue";
-    lang_loading = "Cargando...";
-  } else if (lang == "en") {
-    lang_load = "Load the audio";
-    lang_select = "Select";
-    lang_error = "There was a problem loading the audio\nPlease, reaload the page";
-    lang_start = "Start!";
-    lang_pause = "Pause";
-    lang_continue = "Play";
-    lang_loading = "Loading...";
-  }
-
-  infoLink = select("#sa-info-link");
-  infoLink.position(width-60, extraSpaceH + margin*3.5 + 30);
-  selectMenu = createSelect()
+  ragMenu = createSelect()
     .size(120, 25)
-    .position(margin, margin)
-    .changed(start)
+    .position(extraSpaceW + margin, margin)
+    .changed(startRag)
     .parent("sketch-holder");
-  selectMenu.option(lang_select);
-  var noRec = selectMenu.child();
+  ragMenu.option("Select a rāg");
+  var noRec = ragMenu.child();
   noRec[0].setAttribute("selected", "true");
   noRec[0].setAttribute("disabled", "true");
   noRec[0].setAttribute("hidden", "true");
   noRec[0].setAttribute("style", "display: none");
-  recordingsList = recordingsList.recordingsList;
-  for (var i = 0; i < recordingsList.length; i++) {
-    selectMenu.option(recordingsList[i].selectOption, i);
+  ragList = Object.keys(ragInfo);
+  for (var i = 0; i < ragList.length; i++) {
+    var rag = ragInfo[ragList[i]].name + " " + ragInfo[ragList[i]].nameTrans;
+    ragMenu.option(rag, ragList[i]);
   }
-  buttonPlay = createButton(lang_load)
+
+  ragMenu = createSelect()
     .size(120, 25)
-    .position(width - 120 - margin, margin)
+    .position(extraSpaceW + margin, margin)
+    .changed(startRag)
+    .parent("sketch-holder");
+  ragMenu.option("Select a rāg");
+  var noRec = ragMenu.child();
+  noRec[0].setAttribute("selected", "true");
+  noRec[0].setAttribute("disabled", "true");
+  noRec[0].setAttribute("hidden", "true");
+  noRec[0].setAttribute("style", "display: none");
+  var ragList = Object.keys(ragInfo);
+  for (var i = 0; i < ragList.length; i++) {
+    var rag = ragInfo[ragList[i]].name + " " + ragInfo[ragList[i]].nameTrans;
+    ragMenu.option(rag, ragList[i]);
+  }
+
+  recordingMenu = createSelect()
+    .size(120, 25)
+    // .changed(start)
+    .parent("sketch-holder");
+  recordingMenu.position(width - recordingMenu.width - margin, margin);
+  recordingMenu.option("Select a recording");
+  var noRec = recordingMenu.child();
+  noRec[0].setAttribute("selected", "true");
+  noRec[0].setAttribute("disabled", "true");
+  noRec[0].setAttribute("hidden", "true");
+  noRec[0].setAttribute("style", "display: none");
+  var recList = Object.keys(recordingsList);
+  for (var i = 0; i < recList.length; i++) {
+    var rec = recordingsList[recList[i]].selectOption;
+    recordingMenu.option(rec, recList[i]);
+  }
+
+  buttonPlay = createButton("Load")
+    .size(100, 25)
     .mouseClicked(player)
     .attribute("disabled", "true")
+    .attribute("hidden", "true")
     .parent("sketch-holder");
-
-  showTheka = createCheckbox(' ṭhekā', true)
-    .position(extraSpaceW + margin, talY + talRadius)
-    .parent("sketch-holder");
-  showTal = createCheckbox(' tāl', true)
-    .position(extraSpaceW + margin, showTheka.position()["y"] + showTheka.height)
-    .changed(function() {
-      showTheka.checked(showTal.checked());
-      if (showTal.checked()) {
-        showTheka.removeAttribute("disabled");
-      } else {
-        showTheka.attribute("disabled", "true");
-      }
-    })
-    .parent("sketch-holder");
-  showCursor = createCheckbox(' cursor', true)
-    .position(margin, showTal.position()["y"]+showTal.height)
-    .parent("sketch-holder");
-  showTheka.attribute("disabled", "true");
-  showTheka.attribute("style", "color:rgba(120, 0, 0, 0.5);");
-  showTal.attribute("disabled", "true");
-  showTal.attribute("style", "color:rgba(120, 0, 0, 0.5);");
-  showCursor.attribute("disabled", "true");
-  showCursor.attribute("style", "color:rgba(120, 0, 0, 0.5);");
+  buttonPlay.position(extraSpaceW + margin, height - buttonPlay.height - margin);
 }
 
 function draw () {
@@ -192,23 +158,11 @@ function draw () {
   strokeWeight(5);
   stroke(frontColor);
   fill(backColor);
-  text(rag, extraSpaceW + mainSpace/2, extraSpaceH + margin*3);
+  text(ragName, extraSpaceW + mainSpace/2, extraSpaceH + margin*3);
 
   stroke(0, 50);
   strokeWeight(1);
   line(extraSpaceW + margin*2, extraSpaceH + margin*3 + 30, width - margin*2, extraSpaceH + margin*3 + 30);
-
-  textAlign(CENTER, TOP);
-  stroke(0, 150);
-  strokeWeight(1);
-  textSize(20);
-  fill(0, 150);
-  text(artist, extraSpaceW + mainSpace/2, extraSpaceH + margin*4 + 30);
-
-  // stroke("red");
-  // line(0, cursorTop, width, cursorTop);
-  // stroke("green");
-  // line(0, cursorBottom, width, cursorBottom);
 
   for (var i = 0; i < svaraList.length; i++) {
     svaraList[i].displayLines();
@@ -216,8 +170,6 @@ function draw () {
   for (var i = 0; i < svaraList.length; i++) {
     svaraList[i].displaySvara();
   }
-
-  navBox.displayBack();
 
   if (loaded) {
     navCursor.update();
@@ -239,78 +191,22 @@ function draw () {
       ellipse(melCursorX, cursorY, 5, 5);
     }
   }
+}
 
-  push();
-  translate(talX, talY);
-
-  if (failedLoading) {
-    textAlign(CENTER, CENTER);
-    textSize(15)
-    noStroke()
-    fill(0)
-    text(lang_error, 0, 0);
+function startRag () {
+  var currentRag = ragInfo[ragMenu.value()];
+  pitchSpace = currentRag.pitchSpace;
+  print(pitchSpace);
+  ragName = currentRag.name + " " + currentRag.nameTrans;
+  minHz = pitchSpace[0].cent-100;
+  maxHz = pitchSpace[pitchSpace.length-1].cent+100;
+  svaraList = [];
+  soundList = {};
+  for (var i = 0; i < pitchSpace.length; i++) {
+    var svara = new CreateSvara(pitchSpace[i]);
+    svaraList.push(svara);
   }
-
-  rotate(-90);
-
-  if (loaded) {
-    noFill();
-    stroke(frontColor);
-    strokeWeight(2);
-    ellipse(0, 0, talRadius, talRadius);
-
-    if (showTal.checked()) {
-      shade.update();
-      shade.display();
-    }
-
-    if (currentTal != undefined && showTal.checked()) {
-      var talToDraw = talCircles[currentTal];
-      for (var i = 0; i < talToDraw.strokeCircles.length; i++) {
-        talToDraw.strokeCircles[i].display();
-      }
-      if (showTheka.checked()) {
-        for (var i = 0; i < talToDraw.strokeCircles.length; i++) {
-          talToDraw.strokeCircles[i].displayTheka();
-        }
-        for (var i = 0; i < talToDraw.icons.length; i++) {
-          talToDraw.icons[i].display();
-        }
-      }
-    }
-    // talCursor.update();
-    // talCursor.display();
-  } else {
-    charger.update();
-    charger.display();
-    talCursor.loadingUpdate();
-    talCursor.display();
-  }
-
-  pop();
-
-  if (showTal.checked()) {
-    textAlign(CENTER, CENTER);
-    textSize(20);
-    textStyle(NORMAL);
-    stroke(backColor);
-    strokeWeight(5);
-    fill(frontColor);
-    text(talName, talX, talY);
-  }
-
-  for (var i = 0; i < talBoxes.length; i++) {
-    talBoxes[i].display();
-  }
-
-  textAlign(RIGHT, BOTTOM);
-  textSize(12);
-  textStyle(NORMAL);
-  noStroke();
-  fill(frontColor);
-  text(mpmTxt, extraSpaceW + margin + 65, navBox.y1 - margin/2);
-
-  navBox.displayFront();
+  buttonPlay.attribute("hidden", true);
 }
 
 function start () {
@@ -363,18 +259,6 @@ function start () {
   currentAvart = new CreateCurrentAvart();
   shade = new CreateShade();
   clock = new CreateClock;
-
-  showTheka.attribute("disabled", "true");
-  showTheka.attribute("style", "color:rgba(120, 0, 0, 0.5);");
-  showTheka.checked("true");
-  showTal.attribute("disabled", "true");
-  showTal.attribute("style", "color:rgba(120, 0, 0, 0.5);");
-  showTal.checked("true");
-  showCursor.attribute("disabled", "true");
-  showCursor.attribute("style", "color:rgba(120, 0, 0, 0.5);");
-  showCursor.checked("true");
-  buttonPlay.html(lang_load);
-  buttonPlay.removeAttribute("disabled");
 }
 
 function CreateNavigationBox () {
@@ -459,7 +343,6 @@ function CreateSvara (svara) {
   this.x1 = melCursorX;
   this.y = map(svara.cent, minHz, maxHz, cursorBottom, cursorTop);
   this.name = svara.svara;
-  this.key = svara.key;
   this.function = svara.function;
   if (this.function == "sadja") {
     this.radius = svaraRadius1;
@@ -526,279 +409,6 @@ function CreateSvara (svara) {
   }
 }
 
-function createSound (svara) {
-  this.pitch = svara.pitch;
-  this.key = svara.key;
-  this.osc = new p5.Oscillator();
-  this.osc.setType("sawtooth");
-  this.osc.freq(this.pitch);
-  soundList[this.key] = this.osc;
-}
-
-function CreateTalCursor () {
-  this.x;
-  this.y;
-
-  this.update = function () {
-    this.angle = map(currentTime, currentAvart.start, currentAvart.end, 0, 360);
-    this.x = talRadius * cos(this.angle);
-    this.y = talRadius * sin(this.angle);
-  }
-
-  this.loadingUpdate = function () {
-    this.x = talRadius * cos(charger.angle);
-    this.y = talRadius * sin(charger.angle);
-  }
-
-  this.display = function () {
-    fill("red");
-    stroke(frontColor);
-    strokeWeight(1);
-    ellipse(this.x, this.y, 5, 5)
-  }
-}
-
-function CreateShade () {
-  this.x;
-  this.y;
-  this.angle;
-  this.alpha;
-  this.col = shadeColor;
-
-  this.update = function () {
-    if (!(currentTime >= currentAvart.start && currentTime <= currentAvart.end)) {
-      currentAvart.update();
-    }
-    this.angle = map(currentTime, currentAvart.start, currentAvart.end, 0, 360);
-    this.alpha = map(this.angle, 0, 360, 0, 255);
-    this.x = talRadius * cos(this.angle);
-    this.y = talRadius * sin(this.angle);
-  }
-  this.display = function () {
-    this.col.setAlpha(this.alpha);
-    fill(this.col);
-    noStroke();
-    arc(0, 0, talRadius, talRadius, 0, this.angle);
-    stroke(frontColor);
-    strokeWeight(1);
-    line(0, 0, this.x, this.y);
-  }
-}
-
-function CreateCurrentAvart () {
-  this.index;
-  this.tal;
-  this.sam;
-  this.start;
-  this.end;
-  this.findIndex = function () {
-    while (currentTime > this.sam[this.index+1]) {
-      this.index++;
-    }
-    while (currentTime < this.sam[this.index]) {
-      this.index--;
-    }
-  }
-  this.update = function () {
-    if (currentTal == undefined) {
-      this.start = undefined;
-      this.end = undefined;
-      mpmTxt = undefined;
-    } else {
-      if (this.tal == currentTal) {
-        this.findIndex();
-      } else {
-        this.tal = currentTal
-        this.sam = talList[this.tal].sam;
-        this.index = 0;
-        this.findIndex();
-      }
-      this.start = this.sam[this.index];
-      this.end = this.sam[this.index+1];
-      var mpm = 60 / ((this.end - this.start) / 10);
-      mpmTxt = str(mpm.toFixed(1)) + " mpm"
-    }
-  }
-}
-
-function CreateTalCircle (tal) {
-  this.strokeCircles = [];
-  this.icons = [];
-  this.avart;
-
-  var tal = talInfo[tal];
-  this.avart = tal.avart;
-  var theka = tal.theka;
-  for (var i = 0; i < theka.length; i++) {
-    var stroke = theka[i];
-    var matra = stroke.matra;
-    var vibhag; //tali or khali
-    if (int(stroke.vibhag) > 0) {
-      vibhag = "tali";
-    } else {
-      vibhag = "khali";
-    }
-    var circleType;
-    if (i == 0) {
-      circleType = "sam";
-      var icon = new CreateIcon(matra, vibhag, this.avart);
-      this.icons.push(icon);
-    } else if ((stroke.vibhag % 1) < 0.101) {
-      circleType = 1;
-      var icon = new CreateIcon(matra, vibhag, this.avart);
-      this.icons.push(icon);
-    } else if ((stroke.vibhag * 10 % 1) == 0) {
-      circleType = 2;
-    } else {
-      circleType = 3;
-    }
-    var bol = stroke.bol;
-    var strokeCircle = new CreateStrokeCircle(matra, vibhag, circleType, bol, this.avart);
-    this.strokeCircles.push(strokeCircle);
-  }
-}
-
-function CreateStrokeCircle (matra, vibhag, circleType, bol, avart) {
-  this.bol = bol;
-  var increment = 1;
-  this.strokeWeight = 2;
-  this.txtW = 0;
-
-  if (circleType == "sam") {
-    if (vibhag == "tali") {
-      this.col = frontColor;
-      this.txtCol = backColor;
-    } else {
-      this.col = backColor;
-      this.txtCol = frontColor;
-    }
-  } else if (vibhag == "tali") {
-    this.col = frontColor;
-    this.txtCol = backColor;
-  } else if (vibhag == "khali") {
-    this.col = backColor;
-    this.txtCol = frontColor;
-  }
-
-  if (circleType == "sam") {
-    this.radius = strokeRadius1;
-    this.txtSize = strokeRadius1 * 0.7;
-    this.txtStyle = BOLD;
-    this.bol = this.bol.toUpperCase();
-  } else if (circleType == 1) {
-    this.radius = strokeRadius1;
-    this.txtSize = strokeRadius1 * 0.75;
-    this.txtStyle = BOLD;
-  } else if (circleType == 2){
-    this.radius = strokeRadius2;
-    this.txtSize = strokeRadius2 * 0.75;
-    this.txtStyle = BOLD;
-  } else {
-    this.radius = strokeRadius2;
-    this.txtSize = strokeRadius2 * 0.75;
-    this.col = color(0, 0);
-    this.txtCol = frontColor;
-    this.txtStyle = NORMAL;
-    this.strokeWeight = 0;
-    this.txtW = 1;
-    increment = 1.05;
-  }
-
-  this.circleAngle = map(matra, 0, avart, 0, 360);
-  this.x = talRadius * increment * cos(this.circleAngle);
-  this.y = talRadius * increment * sin(this.circleAngle);
-
-  this.display = function () {
-    push();
-    translate(this.x, this.y);
-    stroke(frontColor);
-    strokeWeight(this.strokeWeight);
-    fill(this.col);
-    ellipse(0, 0, this.radius, this.radius);
-    pop();
-  }
-
-  this.displayTheka = function () {
-    push();
-    translate(this.x, this.y);
-    textAlign(CENTER, CENTER);
-    stroke(backColor);
-    strokeWeight(this.txtW);
-    fill(this.txtCol);
-    textSize(this.txtSize);
-    textStyle(this.txtStyle);
-    rotate(90);
-    text(this.bol, 0, 0);
-    pop();
-  }
-}
-
-function CreateIcon (matra, vibhag, avart) {
-  this.circleAngle = map(matra, 0, avart, 0, 360);
-  this.x = talRadius * iconDistance * cos(this.circleAngle);
-  this.y = talRadius * iconDistance * sin(this.circleAngle);
-  if (vibhag == "tali") {
-    this.img = clap;
-  } else if (vibhag == "khali") {
-    this.img = wave;
-  }
-
-  this.display = function () {
-    push();
-    translate(this.x, this.y);
-    rotate(90);
-    image(this.img, 0, 0, strokeRadius2*2, strokeRadius2*2);
-    pop();
-  }
-}
-
-function CreateTalBox (tal) {
-  if (tal.tal[tal.tal.length-1] == 'l') {
-    this.tal = tal.tal;
-  } else {
-    this.tal = tal.tal.slice(0, tal.tal.length-1);
-  }
-  this.talIndex = tal.tal;
-  this.name = talInfo[this.tal].nameTrans;
-  this.fullName = talInfo[this.tal].name + "\n" + this.name;
-  this.h = 25;
-  this.x1 = map(tal.start, 0, trackDuration, navBox.x1+navCursorW/2, navBox.x2-navCursorW/2);
-  this.x2 = map(tal.end, 0, trackDuration, navBox.x1+navCursorW/2, navBox.x2-navCursorW/2);
-  this.w = this.x2-this.x1;
-  this.boxCol = color(255, 100);
-  this.txtBorder = 0;
-  this.txtCol = color(75);
-  this.txtStyle = NORMAL;
-  this.currentSamIndex = 0;
-
-  this.off = function () {
-    this.boxCol = color(255, 100);
-    this.txtCol = color(75);
-    this.txtBorder = 0;
-    this.txtStyle = NORMAL;
-  }
-
-  this.on = function () {
-    this.boxCol = backColorTrans;
-    this.txtCol = color(255);
-    this.txtBorder = 2;
-    this.txtStyle = NORMAL;
-  }
-
-  this.display = function () {
-    fill(this.boxCol);
-    noStroke();
-    rect(this.x1, navBox.y1, this.w, this.h);
-    textAlign(LEFT, BOTTOM);
-    textSize(this.h * 0.7);
-    fill(this.txtCol);
-    textStyle(this.txtStyle);
-    stroke(0);
-    strokeWeight(this.txtBorder);
-    text(this.name, this.x1+2, navBox.y1 + this.h*0.92);
-  }
-}
-
 function CreateClock () {
   this.clock;
   this.total = niceTime(trackDuration);
@@ -843,30 +453,11 @@ function player () {
   }
 }
 
-function CreateCharger () {
-  this.angle;
-  this.update = function () {
-    this.angle += 1;
-  }
-  this.display = function () {
-    stroke(frontColor);
-    strokeWeight(2);
-    noFill();
-    arc(0, 0, talRadius, talRadius, 0, this.angle);
-  }
-}
-
 function soundLoaded () {
   buttonPlay.html(lang_start);
   buttonPlay.removeAttribute("disabled");
   selectMenu.removeAttribute("disabled");
   loaded = true;
-  showTheka.removeAttribute("disabled");
-  showTheka.attribute("style", "color:rgba(120, 0, 0);");
-  showTal.removeAttribute("disabled");
-  showTal.attribute("style", "color:rgba(120, 0, 0);");
-  showCursor.removeAttribute("disabled");
-  showCursor.attribute("style", "color:rgba(120, 0, 0);");
   var endLoading = millis();
   print("Track loaded in " + (endLoading-initLoading)/1000 + " seconds");
 }
@@ -874,21 +465,12 @@ function soundLoaded () {
 function failedLoad () {
   print("Loading failed");
   failedLoading = true;
-  charger.angle = undefined;
 }
 
 function mouseClicked () {
   if (loaded) {
     navBox.clicked();
   }
-}
-
-function keyPressed () {
-  soundList[key.toLowerCase()].start();
-}
-
-function keyReleased () {
-  soundList[key.toLowerCase()].stop();
 }
 
 function niceTime (seconds) {
